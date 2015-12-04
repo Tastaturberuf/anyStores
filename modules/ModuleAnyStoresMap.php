943cd3cb84299b2a9ad21fcd6e42f13b692ad4d5
Daniel Jahnsmüller <mail@tastaturberuf.de>
2015-12-04 17:41:29 +0100

    remove unused code

--- modules/ModuleAnyStoresMap.php
+++ modules/ModuleAnyStoresMap.php
@@ -4,9 +4,7 @@
  * anyStores for Contao Open Source CMS
  *
  * @copyright   (c) 2015 Tastaturberuf <mail@tastaturberuf.de>
- *              (c) 2013, 2014 CLICKPRESS Internetagentur <www.clickpress.de>
  * @author      Daniel Jahnsmüller <mail@jahnsmueller.net>
- *              Stefan Schulz-Lauterbach <ssl@clickpress.de>
  * @license     http://opensource.org/licenses/lgpl-3.0.html
  * @package     anyStores
  */
@@ -15,7 +13,7 @@
 namespace Tastaturberuf;
 
 
-class ModuleAnyStoresMap extends \Module // extends ModuleStoreLocatorList
+class ModuleAnyStoresMap extends \Module
 {
 
     /**
@@ -57,71 +55,6 @@ class ModuleAnyStoresMap extends \Module // extends ModuleStoreLocatorList
         $GLOBALS['TL_JAVASCRIPT']['googleapis-maps'] = 'https://maps.googleapis.com/maps/api/js?sensor=false&amp;language='.$GLOBALS['TL_LANGUAGE'];
         $GLOBALS['TL_JAVASCRIPT']['anystores']       = 'system/modules/anyStores/assets/js/anystores.js';
         $GLOBALS['TL_JAVASCRIPT']['markerclusterer'] = 'system/modules/anyStores/assets/js/markerclusterer/markerclusterer_compiled.js';
-
-        // get published stores from categories
-        $objStores = AnyStoresModel::findPublishedByCategory(deserialize($this->anystores_categories));
-
-        // return if no stores found
-        if ( !$objStores )
-        {
-            return;
-        }
-
-        // generate entries
-        while ( $objStores->next() )
-        {
-            // generate jump to
-            //@todo copy do AnyStoresModule and extends from it
-            if ( $this->jumpTo )
-            {
-                if ( ($objLocation = \PageModel::findByPk($this->jumpTo)) !== null )
-                {
-                    //@todo language parameter
-                    $strStoreKey   = !$GLOBALS['TL_CONFIG']['useAutoItem'] ? '/store/' : '/';
-                    $strStoreValue = $objStores->alias;
-
-                    $objStores->href = \Controller::generateFrontendUrl(
-                        $objLocation->row(),
-                        $strStoreKey.$strStoreValue
-                    );
-                }
-            }
-
-            // unset logo because json_encode drops error:
-            // Malformed UTF-8 characters, possibly incorrectly encoded
-            //@todo maybe ->loadDetails() fix it
-            $objStores->logo = null;
-
-            // encode email
-            $objStores->email = \String::encodeEmail($objStores->email);
-        }
-
-        // get all stores
-        $arrStores = $objStores->fetchAll();
-
-        $this->Template->entries = $arrStores;
-
-        if ( !$strJson = json_encode($arrStores) )
-        {
-            $this->log(json_last_error_msg(), __METHOD__, TL_ERROR);
-            //@todo stop display the broken module
-            return;
-        }
-
-        // Temporär
-        $this->Template->json = $strJson;
-
-
-        //@todo cleanup
-        $path = 'system/modules/anyStores/html/' . $this->id . '-locations.json';
-        $this->Template->path = $path;
-
-        //JSON schreiben
-        //@todo: Muss noch in tl_storelocator_map zum save_callback verschoben werden!!!
-        #$file   =   new \File($path);
-        #$file->write($strJson);
-        //@todo language
-        #$this->log('Neue JSON-Datei erstellt', __METHOD__, TL_FILES);
     }
 
 }
